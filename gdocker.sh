26cb51c5f80ba270bc3112f42cd9407468cde17f
xiongyu <xiongyu@espressif.com>
2021-04-06 21:39:22 +0800

    fix docker in docker

--- gdocker.sh
+++ gdocker.sh
@@ -117,9 +117,27 @@ function install(){
     sudo docker rm ${IMAGE_NAME} 2> /dev/null
 
     if [[ "${TOOL_RELPATH}" == "" ]]; then
-        sudo docker run --name ${IMAGE_NAME} -i -e MY_NAME=${IMAGE_NAME} -v ${GIT_PATH}:/home/${IMAGE_NAME}/workspace "${IMAGE_NAME}:${IMAGE_VERSION}" bash
+        sudo docker run \
+            --name ${IMAGE_NAME} \
+            -i \
+            -e MY_NAME=${IMAGE_NAME} \
+            \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            -v $(which docker):/bin/docker \
+            -v ${GIT_PATH}:/home/${IMAGE_NAME}/workspace \
+            \
+            "${IMAGE_NAME}:${IMAGE_VERSION}" bash
     else
-        sudo docker run --name ${IMAGE_NAME} -i -e MY_NAME=${IMAGE_NAME} -v ${GIT_PATH}:/home/${IMAGE_NAME}/workspace "${IMAGE_NAME}:${IMAGE_VERSION}" /bin/bash /home/${IMAGE_NAME}/workspace/${TOOL_RELPATH}
+        sudo docker run \
+            --name ${IMAGE_NAME} \
+            -i \
+            -e MY_NAME=${IMAGE_NAME} \
+            \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            -v $(which docker):/bin/docker \
+            -v ${GIT_PATH}:/home/${IMAGE_NAME}/workspace \
+            \
+            "${IMAGE_NAME}:${IMAGE_VERSION}" /bin/bash /home/${IMAGE_NAME}/workspace/${TOOL_RELPATH}
     fi
     sudo docker commit -m "install ok" ${IMAGE_NAME} "${IMAGE_NAME}:${IMAGE_VERSION}"
     sudo docker rm ${IMAGE_NAME}
@@ -186,6 +204,8 @@ function start(){
         -e GTK_IM_MODULE=${GTK_IM_MODULE} \
         -e QT_IM_MODULE=${QT_IM_MODULE} \
         \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v $(which docker):/bin/docker \
         -v /dev:/dev \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         -v $HOME/.Xauthority:/home/${IMAGE_NAME}/.Xauthority \
